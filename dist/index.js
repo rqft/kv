0a2bd55a96400bd1ec944ce4b954295fabbf55b3
Arcy <higharcswastaken@gmail.com>
2022-09-07 17:02:13 -0500

    a

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,5 @@
-export { Wilson } from "./kv";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Wilson = void 0;
+var kv_1 = require("./kv");
+Object.defineProperty(exports, "Wilson", { enumerable: true, get: function () { return kv_1.Wilson; } });
